76538e15b77badb3011b3bab5af69c674d6657c2
Er-Shaikh <90636752+Er-Shaikh@users.noreply.github.com>
2023-01-11 20:22:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let nav = document.querySelector('nav');
+let scrollBtn = document.querySelector('.scroll-up-btn a')
+
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add('sticky');
+        scrollBtn.style.display = 'block';
+    }else{
+        nav.classList.remove('sticky');
+        scrollBtn.style.display = 'none';
+    }
+}
